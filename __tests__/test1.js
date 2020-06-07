d31d52c9b1c6d5b96d1249e37bbe9048f0d7f576
k-k-d <krishnadhakshin2000@gmail.com>
2020-06-07 19:36:20 +0530

    Comments

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -7,12 +7,12 @@ describe('', () => {
 
     let accessToken, refreshToken, authToken;
 
-    it ('Successful Ping', () => {
+    it ('Successful Ping', () => {  // 0 - testing ping api
         return frisby
-            .get(baseUrl + '/users/ping')
-            .expect('status', 200)
-            .expect('jsonTypes', {
-                'tenant': Joi.string().required(),
+            .get(baseUrl + '/users/ping')   // get request
+            .expect('status', 200)  // check status code
+            .expect('jsonTypes', {  // check response json template
+                'tenant': Joi.string().required(),  // use joi to make a schema of the expected response
                 'status': Joi.string().valid('ok').required(),
                 'service': Joi.string().required(),
                 'a': Joi.number().required(),
@@ -25,7 +25,7 @@ describe('', () => {
         ;
     });
 
-    frisby.globalSetup({
+    frisby.globalSetup({    // setup headers for all future tests
         request: {
             headers: {
                 'Content-Type': 'application/json',
@@ -35,16 +35,16 @@ describe('', () => {
 
     it('Login API returns the 3 tokens for authorization', () =>{   // 1 - testing login with email api
         return frisby
-            .post(baseUrl + '/users/auth/login', {
+            .post(baseUrl + '/users/auth/login', {  // post request with payload
                 username: "krishna@betalectic.com",
                 password: "Krishna@09"
             })
-            .expect('status', 200)
-            .expect('jsonTypes', {
+            .expect('status', 200)  // check status code
+            .expect('jsonTypes', {  // check response json template with joi
                 'ChallengeParameters': Joi.object().required(),
                 'AuthenticationResult': Joi.object().required()
             })
-            .expect('jsonTypes', 'AuthenticationResult', {
+            .expect('jsonTypes', 'AuthenticationResult', {  // check template of specific parameter of response again using joi schema
                 "AccessToken": Joi.string().required(),
                 "RefreshToken": Joi.string().required(),
                 "IdToken": Joi.string().required(),
@@ -61,7 +61,7 @@ describe('', () => {
 
     it('User details returns the correct data', () => { // 2 - testing get user api
         return frisby
-            .setup({
+            .setup({    // set up headers with authentication details before sending requests
                 request: {
                     headers: {
                         'Content-Type': 'application/json',
@@ -71,9 +71,9 @@ describe('', () => {
                     }
                 }
             })
-            .get(baseUrl + '/users/account/me')
-            .expect('status', 200)
-            .expect('jsonTypes', {
+            .get(baseUrl + '/users/account/me') // get request
+            .expect('status', 200)  // check status code
+            .expect('jsonTypes', {  // check if response compares with expected template
                 'full_name': Joi.string().required()
             })
         ;
